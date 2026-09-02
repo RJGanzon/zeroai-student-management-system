b778ee2de572c5d947a577122a49904c9c9a5cb5
RJGanzon <ganzonralpht@gmail.com>
2026-09-02 23:50:21 +0800

    fetchAllBooks unit test and bookServiceImpl injection bug fix.

--- src/test/java/com/week9/study/serviceTests/implTests/BookServiceImplTests.java
+++ src/test/java/com/week9/study/serviceTests/implTests/BookServiceImplTests.java
@@ -3,19 +3,23 @@ package com.week9.study.serviceTests.implTests;
 import com.week9.study.dto.BookDto;
 import com.week9.study.dto.summaries.BookSummaryDto;
 import com.week9.study.entities.BookEntity;
+import com.week9.study.mapper.impl.BookMapperImpl;
 import com.week9.study.mapper.impl.summaries.BookSummaryMapperImpl;
+import com.week9.study.mapper.impl.summaries.StudentSummaryMapperImpl;
 import com.week9.study.repositories.BookRepository;
 import com.week9.study.services.impl.BookServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,6 +28,8 @@ public class BookServiceImplTests {
     BookSummaryDto bookSummaryDto;
     BookDto bookDto;
     BookEntity bookEntity;
+    List<BookEntity> bookEntityList;
+    List<BookSummaryDto> bookSummaryDtoList;
 
     @Mock
     private BookRepository bookRepository;
@@ -31,11 +37,18 @@ public class BookServiceImplTests {
     @Mock
     private BookSummaryMapperImpl bookSummaryDtoMapper;
 
-    @InjectMocks
+    @Mock
+    private BookMapperImpl bookDtoMapper;
+
+    @Mock
+    private StudentSummaryMapperImpl studentSummaryMapper;
+
+//    @InjectMocks
     private BookServiceImpl bookServiceImpl;
 
     @BeforeEach
     void setup() {
+        bookServiceImpl = new BookServiceImpl(bookDtoMapper, bookSummaryDtoMapper, bookRepository, studentSummaryMapper);
         bookEntity = BookEntity.builder()
                 .isbn("978-1408856772")
                 .title("Harry Potter")
@@ -49,7 +62,11 @@ public class BookServiceImplTests {
         bookDto = BookDto.builder()
                 .isbn("978-1408856772")
                 .title("Harry Potter")
+                .studentId(null)
                 .build();
+
+        bookEntityList = List.of(bookEntity, bookEntity);
+        bookSummaryDtoList = List.of(bookSummaryDto, bookSummaryDto);
     }
 
     //Save a Book
@@ -59,22 +76,30 @@ public class BookServiceImplTests {
         //mock methods
         when(this.bookSummaryDtoMapper.mapFrom(bookSummaryDto)).thenReturn(bookEntity);
         when(this.bookRepository.save(bookEntity)).thenReturn(bookEntity);
+        when(this.bookDtoMapper.mapTo(bookEntity)).thenReturn(bookDto);
 
-        //test implementation
-        BookEntity bookEntityMock = bookSummaryDtoMapper.mapFrom(bookSummaryDto);
-        BookEntity savedBookEntity = bookRepository.save(bookEntityMock);
+        //Call actual method
+        BookDto result = bookServiceImpl.saveBook(bookSummaryDto);
 
         //asserts
-        assertThat(bookEntity,equalTo(savedBookEntity));
+        assertThat(result,equalTo(bookDto));
+        verify(this.bookRepository).save(bookEntity);
     }
 
-    //Save Exception Test
+    //Fetch All Books Test
     @Test
-    @DisplayName("Save Book Exception Successful")
-    public void saveBookNullException() {
+    @DisplayName("Books Fetch Successful")
+    public void fetchAllBooksTest() {
+        //mock methods
+        when(this.bookRepository.findAll()).thenReturn(bookEntityList);
+        when(this.bookSummaryDtoMapper.mapTo(bookEntity)).thenReturn(bookSummaryDto);
 
+        //Call actual method
+        List<BookSummaryDto> result = bookServiceImpl.fetchAllBooks();
+
+        //asserts
+        assertThat(result, equalTo(bookSummaryDtoList));
     }
 
-    //
 
 }
